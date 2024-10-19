2af96f076e1d6683dbcc3fa1ea9b178b9562d4f2
Makardhwaj <mhs93679@gmail.com>
2024-10-19 13:05:30 +0530

    added a header content

---
+++ components/home/headerName.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function HeaderName():React.JSX.Element{
+    return(
+        <View style={styles.mainheader}>
+           <View style={styles.maintitle}>
+           <Text style={styles.title1}>
+                READY TO
+            </Text>
+            <Text style={styles.title2}>
+                 WORKOUT
+            </Text>
+           </View>
+           <View style={styles.section2}>
+            <View style={styles.profile}></View>
+            <View style={styles.noti}></View>
+           </View>
+        </View>
+    )
+}
+
+const styles=StyleSheet.create({
+    mainheader:{
+      flexDirection:'row',
+      justifyContent:'space-between',
+      marginTop:10
+    },
+    maintitle:{
+      marginLeft:20,
+    },
+    title1:{
+      fontSize:35,
+      color:'black',
+      textDecorationStyle:'solid'
+    },
+    title2:{
+        fontSize:35,
+        color:'red',
+        textDecorationStyle:'solid'
+    },
+    section2:{
+      marginRight:15,
+    },
+    profile:{
+         width:40,
+         height:40,
+         backgroundColor:'black',
+         borderRadius:25,
+         marginBottom:7
+    },
+    noti:{
+        width:40,
+        height:40,
+        backgroundColor:'black',
+        borderRadius:25
+    }
+})
---
+++ components/home/home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "react-native";
+import HeaderName from "./headerName";
+export default function Home():React.JSX.Element{
+    return(
+        <View>
+              <HeaderName/>
+        </View>
+    )
+}
