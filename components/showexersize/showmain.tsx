83fac76c7afaa5ac76f0a661c162e42bd2ce2842
Makardhwaj <mhs93679@gmail.com>
2024-10-20 19:25:37 +0530

    designed full show page

--- components/showexersize/showmain.tsx
+++ components/showexersize/showmain.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet,TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet,TouchableOpacity, ScrollView } from "react-native";
 import FastImage from "react-native-fast-image";
 import { RouteProp } from '@react-navigation/native';
 import { useNavigation } from "@react-navigation/native";
@@ -15,9 +15,8 @@ interface ShowMainProps {
 export default function ShowMain({ route }: ShowMainProps): React.JSX.Element {
   const navigation=useNavigation()
   const { item } = route.params;
-
   return (
-    <View>
+    <View style={{flex:1}}>
         <FastImage
             style={styles.showgif}
             source={{ uri: item.gifUrl }}
@@ -26,6 +25,23 @@ export default function ShowMain({ route }: ShowMainProps): React.JSX.Element {
         <TouchableOpacity style={styles.closebtn} onPress={()=>navigation.goBack()}>
                 <Icon style={styles.closeicon} name="cross"/></TouchableOpacity>
                 <Text style={styles.showexrsizetext}>{item.name} </Text>
+        <ScrollView style={{marginTop:20,marginBottom:20}}>
+            <Text style={styles.outertxt}>Equipment <Text style={styles.innertxt}>{item.equipment}</Text></Text>
+            <Text style={styles.outertxt}>Secondary Muscules 
+            {
+            item.secondaryMuscles && item.secondaryMuscles.length>0 && item.secondaryMuscles.map((val:string)=>(
+                 <Text style={styles.innertxt}> {val},</Text>
+            ))
+            }
+            </Text>
+            <Text style={styles.outertxt}>Target <Text style={styles.innertxt}>{item.target}</Text></Text>
+            <Text style={styles.instrmain}>Instruction</Text>
+            {
+                 item.instructions && item.instructions.length>0 && item.instructions.map((val:string)=>(
+                    <Text style={styles.instrtext} > {val},</Text>
+               ))
+            }
+        </ScrollView>
     </View>
   );
 }
@@ -57,5 +73,30 @@ showexrsizetext:{
     color:'black',
     fontFamily:'Ubuntu-Medium',
     marginTop:10
+},
+outertxt:{
+marginLeft:25,
+marginBottom:5,
+fontSize:16,
+color:'black',
+fontFamily:'Ubuntu-Light'
+},
+innertxt:{
+fontFamily:'Ubuntu-Bold',
+},
+instrmain:{
+    fontSize:22,
+    fontFamily:'Ubuntu-Medium',
+    marginLeft:15,
+    marginTop:10,
+    marginBottom:10,
+    color:'black'
+},
+instrtext:{
+    marginBottom:7,
+    color:'black',
+    marginLeft:20,
+    fontSize:15,
+    fontFamily:'Ubuntu-Light'
 }
 })
