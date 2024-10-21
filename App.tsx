ff5c91bc694c59be419f3864f8e249fa6aff17ba
Makardhwaj <mhs93679@gmail.com>
2024-10-21 14:28:50 +0530

    added notification when anyone opens app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,38 @@
 
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Text, View } from 'react-native';
 import Home from './components/home/home';
 import Nav from './components/navigation/navigation';
+import notifee from '@notifee/react-native';
 
 function App(): React.JSX.Element {
+
+  async function onDisplayNotification() {
+    // Request permissions (required for iOS)
+    await notifee.requestPermission()
+
+    // Create a channel (required for Android)
+    const channelId = await notifee.createChannel({
+      id: 'default',
+      name: 'Default Channel',
+    });
+
+    await notifee.displayNotification({
+      title: 'hey,Welcome to fitness club',
+      body: 'here you can check all fitness options..',
+      android: {
+        channelId,
+        pressAction: {
+          id: 'default',
+        },
+      },
+    });
+  }
+  useEffect(()=>{
+    onDisplayNotification()
+  },[])
+
   return(
      <Nav/>
   )
